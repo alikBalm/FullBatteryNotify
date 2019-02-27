3a1c43277b040f268a905ae540a40a71fe6aa87e
alik <alik@oberon-alfa.ru>
2019-02-27 15:04:47 +0300

    Над этим ещё работать и работать

--- app/src/main/java/com/alikbalm/fullbatterynotify/MainActivity.java
+++ app/src/main/java/com/alikbalm/fullbatterynotify/MainActivity.java
@@ -1,17 +1,21 @@
 package com.alikbalm.fullbatterynotify;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button start, stop, status;
+    Button start, stop, status, play, pause;
     static TextView textStatus, textPercent, statusText;
-    //ConstraintLayout constarint;
+    //ConstraintLayout constraint;
+
+    MediaPlayer mediaPlayer;
 
 
     @Override
@@ -21,19 +25,27 @@ public class MainActivity extends AppCompatActivity {
 
         // страртуем сервис при запуске приложения, после чего пользователь
         // может выходить из приложения, а служба будет работать в фоне
-        startService(new Intent(MainActivity.this,MyService.class));
+        //startService(new Intent(MainActivity.this,MyService.class));
 
         //constarint = (ConstraintLayout) findViewById(R.id.constarint);
+
+//        mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.batareya_sveta);
+
+
+
+
         getSupportActionBar().hide();
 
-        start = (Button) findViewById(R.id.start);
-        stop = (Button) findViewById(R.id.stop);
-        status = (Button) findViewById(R.id.status);
+        start = findViewById(R.id.start);
+        stop = findViewById(R.id.stop);
+        status = findViewById(R.id.status);
+        play = findViewById(R.id.play);
+        pause = findViewById(R.id.pause);
 
-        statusText = (TextView) findViewById(R.id.statusText);
+        statusText = findViewById(R.id.statusText);
 
-        textStatus = (TextView) findViewById(R.id.textStatus);
-        textPercent = (TextView) findViewById(R.id.textPercent);
+        textStatus = findViewById(R.id.textStatus);
+        textPercent = findViewById(R.id.textPercent);
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,6 +75,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (mediaPlayer!=null ) mediaPlayer.stop();
+
+                mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.facebook_ringtone_pop);
+                mediaPlayer.start();
+
+
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (mediaPlayer!=null ) {mediaPlayer.stop();}
+                else {
+                    Log.i(" mediaplayer stop", " == null");
+                }
+
+            }
+        });
 
     }
 }
--- app/src/main/java/com/alikbalm/fullbatterynotify/MyService.java
+++ app/src/main/java/com/alikbalm/fullbatterynotify/MyService.java
@@ -13,29 +13,29 @@ import android.util.Log;
 
 public class MyService extends Service {
 
-    MediaPlayer mp;
 
+    MediaPlayer mp;
     static boolean myServiceIsActive = false;
+    int idRaw =R.raw.batareya_sveta;
+//    boolean charging = false;
 
     public MyService() {
     }
 
     @Override
     public void onCreate() {
-       // Log.i("My Service", " onCreate");
-
-        //mp = MediaPlayer.create(this, R.raw.batareya_sveta);
-        //mp.setLooping(true);
 
         //регистрируем ресивер
         this.registerReceiver(this.mBatInfoReciever,new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
+        mp = MediaPlayer.create(getApplicationContext(),R.raw.facebook_ringtone_pop);
+
 
         super.onCreate();
     }
 
     @Override
     public void onDestroy() {
-        //Log.i("My Service", " onDestroy");
         myServiceIsActive = false;
 
         //удаляем регистрацию ресивера
@@ -55,89 +55,57 @@ public class MyService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-        //Log.i("My Service", " onStartCommand");
         myServiceIsActive = true;
-        //watchCharge();
+
         return super.onStartCommand(intent, flags, startId);
     }
 
     // сам ресивер
-    private BroadcastReceiver mBatInfoReciever = new BroadcastReceiver() {
+     private BroadcastReceiver mBatInfoReciever = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
 
             //делаем что то когда получено уведомление о заряде
             //здесь нужно запустиь mp.start() если заряжается, батарея заряжена на 100%
             // апосле отсоединения mp.stop()
+
+            //int tempInt = 0;
             int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
             int percent = intent.getIntExtra(BatteryManager.EXTRA_LEVEL,0);
             if (status==BatteryManager.BATTERY_STATUS_CHARGING) {
+                //charging = true;
+
+                //Toast.makeText(context, " Connected ", Toast.LENGTH_SHORT).show();
                 //MainActivity.textStatus.setText(String.valueOf("CHARGING"));
                 // здесь код для проигрыша звука при зарядке, и 100 % заряда предположительно
-                if (percent==100) {
+
 
                 //предположительно при лупинге тру
+                    Log.i(" charging ", String.valueOf(percent));
 
-                    /*mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                        @Override
-                        public void onPrepared(MediaPlayer mediaPlayer) {
-                            mediaPlayer.start();
-                        }
-                    });*/
-                    //mp.prepareAsync();
-                    mp = MediaPlayer.create(MyService.this,R.raw.batareya_sveta);
-                    mp.setLooping(true);
+
+                    // нужно копать MediaPlayer
                     mp.start();
 
-                }
 
 
             } else if (status == BatteryManager.BATTERY_STATUS_DISCHARGING){
                 //MainActivity.textStatus.setText(String.valueOf("DISCHARGING"));
                 // здесь код для того чтоб остановить звуковоспроизведение,
                 // т.е. когда отключат от зарядки
-                if (mp != null) {
+                //charging = false;
 
-                    mp.stop();
-                    mp.release();
-                }
+                Log.i(" discharging", String.valueOf(percent));
+
+
+                mp.release();
+
             }
-
-            //MainActivity.textPercent.setText(String.valueOf(percent));
-
-
-            /*if (status == BatteryManager.BATTERY_STATUS_CHARGING) {
-
-                int percent = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
-                if (percent == 100) {
-                    //mp.start();
-                }
-            } else {
-                //mp.stop();
-            }*/
-
-            //int percent = intent.getIntExtra(BatteryManager.EXTRA_LEVEL,0);
-
-
-            //MainActivity.textPercent.setText(String.valueOf(percent));
-
-
 
         }
     };
 
-    /*private void watchCharge() {
-        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-
-        Intent batteryStatus = getApplicationContext().registerReceiver(null, ifilter);
-
-        // Are we charging and charged?
-        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
 
 
-        MainActivity.statusText.setText(String.valueOf(BatteryManager.BATTERY_STATUS_CHARGING));
-        boolean isChargingAndCharged = status == BatteryManager.BATTERY_STATUS_CHARGING &&
-                                       status == BatteryManager.BATTERY_STATUS_FULL;
 
-    }*/
 }
